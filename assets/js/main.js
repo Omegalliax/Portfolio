c90894eb035157115562f95fa2d1c51acbd9c2ce
Omegalliax <vitoreliel2011@hotmail.com>
2022-07-24 18:47:37 -0300

    Testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,24 +12,24 @@ const modalViews = document.querySelectorAll('.services__modal'),
     modalBtns = document.querySelectorAll('.services__button'),
     modalClose = document.querySelectorAll('.services__modal-close')
 
-    let modal = function(modalClick){
-        modalViews[modalClick].classList.add('active-modal')
-    }
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
 
-    modalBtns.forEach((mb, i) =>{
-        mb.addEventListener('click', () =>{
-            modal(i)
-        })
-    } )
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i)
+    })
+})
 
 
-    modalClose.forEach((mc) =>{
-        mc.addEventListener('click', () => {
-            modalViews.forEach((mv) => {
-                mv.classList.remove('active-modal')
-            })
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
         })
     })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 let mixerPortfolio = mixitup('.work__container', {
@@ -52,7 +52,24 @@ function activeWork() {
 linkWork.forEach(l => l.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
-
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+        },
+    },
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
