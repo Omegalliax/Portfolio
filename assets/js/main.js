db638f3abbc1b4f8639b4292bdbcceac5e4d125d
Omegalliax <vitoreliel2011@hotmail.com>
2022-07-25 23:38:33 -0300

    Breakpoints incomplete

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,21 +75,21 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
 const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
-const scrollY = window.pageYOffset;
+    const scrollY = window.pageYOffset;
 
-sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-    sectionTop = current.offsetTop - 58,
-    sectionId = current.getAttribute("id");
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute("id");
 
-    scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ? 
-    document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link") : 
-    document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
-});
+        scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ?
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link") :
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
+    });
 }
 window.addEventListener("scroll", scrollActive);
 
-/*=============== LIGHT DARK THEME ===============*/
+/*=============== LIGHT THEME ===============*/
 const themeButton = document.getElementById('theme-button')
 const lightTheme = 'light-theme'
 const iconTheme = 'bx-sun'
@@ -98,20 +98,20 @@ const iconTheme = 'bx-sun'
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
+// We obtain the current theme that the interface has by validating the light-theme class
 const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
-  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
+    // Add or remove the light/ icon theme
     document.body.classList.toggle(lightTheme)
     themeButton.classList.toggle(iconTheme)
     // We save the theme and the current icon that the user chose
